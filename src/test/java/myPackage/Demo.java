8b255a74db1bf7932ea7a7d0da96f505222fa6a7
anketScriptsHub <anket.jadhav@scriptshub.net>
2025-02-12 12:50:41 +0530

    Update Demo.java

--- src/test/java/myPackage/Demo.java
+++ src/test/java/myPackage/Demo.java
@@ -15,6 +15,18 @@ public class Demo {
 		System.out.println("Hello World2");
 
 		//Comment
+
+
+
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
+
+		
+		System.out.println("My World");
+		
+
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
 	}
 
 }
