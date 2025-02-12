44d8e62b1de9d0b5c3d89d39070d6e04479d1fc8
anketScriptsHub <anket.jadhav@scriptshub.net>
2025-02-12 14:47:29 +0530

    Update Demo3.java

--- src/test/java/myPackage/Demo3.java
+++ src/test/java/myPackage/Demo3.java
@@ -4,13 +4,12 @@ public class Demo3 {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		System.out.println("Added one method");
-=======
+
 
 		//Some methods needs to add
 		
->>>>>>> f75abd009ac278f38e6a4cb2f2c3dc919920f66c
+
 		
 	}
 
