765fa29e045eeed181289f0e62e7fd3c95f984ab
anketScriptsHub <anket.jadhav@scriptshub.net>
2025-02-12 12:43:40 +0530

    Update Demo.java

--- src/test/java/myPackage/Demo.java
+++ src/test/java/myPackage/Demo.java
@@ -6,6 +6,8 @@ public class Demo {
 
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
 	}
 
 }
