b79a07d5115eb094f3f66f5066f00c4db483eab5
Anket Jadhav <anket.jadhav@scriptshub.net>
2025-02-13 10:45:11 +0530

    commit

--- src/test/java/myPackage/LogInDemo.java
+++ src/test/java/myPackage/LogInDemo.java
@@ -4,14 +4,18 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LogInDemo {
 	
 	public static void main(String[] args) {
 		
+		ChromeOptions options = new ChromeOptions();
 		
-		WebDriver driver = new ChromeDriver();
+		options.addArguments("--headless");
 		
+		WebDriver driver = new ChromeDriver(options);
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 		driver.manage().window().maximize();
