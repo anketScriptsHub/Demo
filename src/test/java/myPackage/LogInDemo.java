fc7645360b5b21fde5653fcd58a4c4ed53ab8b43
Anket Jadhav <anket.jadhav@scriptshub.net>
2025-02-17 11:12:28 +0530

    com

--- src/test/java/myPackage/LogInDemo.java
+++ src/test/java/myPackage/LogInDemo.java
@@ -11,29 +11,7 @@ public class LogInDemo {
 	
 	public static void main(String[] args) {
 		
-		ChromeOptions options = new ChromeOptions();
-		
-	//	options.addArguments("--headless");
-		
-		WebDriver driver = new ChromeDriver(options);
-
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.manage().window().maximize();
-		
-		driver.get("xyz");
-		
-		driver.findElement(By.xpath("//input[@")).sendKeys("ABCD");
-		
-		System.out.println("Done");
-		
-	//	driver.close();
-		
-		
-		System.out.println("Updated code on Friday....!!!!!!");
-		
-		
-		System.out.println("I have made changes second time....!!!!!!");
+	
 		
 	}
 
