956356d044dd47a737aa970ce0e8ecb7bb3d4faf
Anket Jadhav <anket.jadhav@scriptshub.net>
2025-02-17 11:56:31 +0530

    it worked

--- src/test/java/myPackage/LogInDemo.java
+++ src/test/java/myPackage/LogInDemo.java
@@ -8,17 +8,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LogInDemo {
-	
+
 	public static void main(String[] args) {
-		
-	
 
+		ChromeOptions options = new ChromeOptions();
 
-		
-		eefef4f
-		System.out.println("Here we go....!!!!");
-		
-		
+		// options.addArguments("--headless");
+
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.manage().window().maximize();
+
+		driver.get("xyz");
+
+		driver.findElement(By.xpath("//input[@")).sendKeys("ABCD");
+
+		System.out.println("Done");
+
+		// driver.close();
+
+		System.out.println("Updated code on Friday....!!!!!!");
+
+		System.out.println("I have made changes second time....!!!!!!");
+
 	}
 
 }
